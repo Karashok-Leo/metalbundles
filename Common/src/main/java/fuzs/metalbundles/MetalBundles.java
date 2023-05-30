366b13030fc6cd5cd3c4e669873bc7da1055d17c
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-05-30 21:37:06 +0200

    initial setup

---
+++ Common/src/main/java/fuzs/metalbundles/MetalBundles.java
@@ -0,0 +1,16 @@
+package fuzs.metalbundles;
+
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetalBundles implements ModConstructor {
+    public static final String MOD_ID = "metalbundles";
+    public static final String MOD_NAME = "Metal Bundles";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+}
---
+++ Common/src/main/java/fuzs/metalbundles/client/MetalBundlesClient.java
@@ -0,0 +1,7 @@
+package fuzs.metalbundles.client;
+
+import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+
+public class MetalBundlesClient implements ClientModConstructor {
+
+}
